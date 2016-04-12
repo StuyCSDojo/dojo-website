61b93f586d83e872a24cd66b22e74235d1e605a8
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-04-12 16:53:19 -0400

    updated

--- api.py
+++ api.py
@@ -20,6 +20,41 @@ from functools import wraps
 from hashlib import sha1
 from sys import argv
 from werkzeug.contrib.fixers import ProxyFix
+from time import gmtime, strftime
+
+def log_name(f):
+    @wraps(f)
+    def inner(*args):
+        print f.func_name + '(' + str(*args) + ')'
+        return f(*args)
+    return inner
+
+def log_time(f):
+    @wraps(f)
+    def inner(*args):
+        init_t = time()
+        ret_val = f(*args)
+        fin_t = time()
+        print "Time: %f" % (fin_t - init_t)
+        return ret_val
+    return inner
+
+admin_accounts = {
+        'brandon' : 'b8a49162dce8ad8afb06a563ea862c04063b6f7a',
+        'sophia': '6f516c4f69481dff8c01aec9da09c620f319cecd',
+        'roz' : '6a5955fc112ecb6adf59c8d6023e1c41e1e9ae83'
+        }
+
+admin_names = {
+        'brandon' : 'Brandon Lin',
+        'sophia' : 'Sophia Zheng',
+        'roz' : 'Roz Joyce'
+        }
+
+def format_announcement(name, title, body):
+    return """<h4>%s</h4>
+<p class='condensed light a_info'>Posted by %s on %s</p>
+<p>%s</p>""" % (title, admin_names[name], strftime('%c', gmtime()), body)
 
 app = Flask(__name__)
 
@@ -33,18 +68,22 @@ def about():
     return render_template("about.html")
 
 @app.route("/resources")
+@log_name
 def resources():
     return render_template("resources.html")
 
 @app.route("/irc")
+@log_name
 def irc():
     return render_template("irc.html")
 
 @app.route('/calendar')
+@log_name
 def calendar():
     return render_template('calendar.html')
 
 @app.route("/tutorials/<tut>")
+@log_name
 def tutorial(tut):
     try:
         return render_template("./tutorials/" + tut)
@@ -52,15 +91,31 @@ def tutorial(tut):
         return render_template('404.html'), 404
 
 @app.route('/admins')
+@log_name
 def admins():
     return render_template('make_announcement_admin.html')
 
 @app.route('/admins', methods = ['POST'])
+@log_name
 def update_announcements():
-# TODO
-    return redirect(url_for('home'));
+    user = request.form['user']
+    if user in admin_accounts:
+        password = sha1()
+        password.update(request.form['pass'])
+        password = password.hexdigest()
+        if admin_accounts[user] == password: # validated
+            # do stuff TODO
+            title = request.form['title']
+            body = request.form['textarea1']
+            oldpage = open('./templates/index.html', 'r').read().split('<h4>')
+            newpage = open('./templates/index.html', 'w')
+            newpage.write(oldpage[0] + format_announcement(user, title, body) + '\n<h4>' + '<h4>'.join(oldpage[1:]))
+            return redirect(url_for('home'));
+
+    return render_template('make_announcement_admin.html', err='Incorrect username/password')
 
 @app.errorhandler(404)
+@log_name
 def page_not_found(error):
     return render_template('404.html'), 404
 
@@ -74,4 +129,5 @@ except ValueError:
 app.debug = True
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    app.run(host='0.0.0.0', port=5000)
