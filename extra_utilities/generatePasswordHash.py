2ce521e568733173c41a343a690fe8b825551acd
Patrick Chan <patrickchan1999@gmail.com>
2017-01-11 21:19:15 -0500

    Added py file for generating hashed password and cleaning up whitespaces in codeblock tags

---
+++ extra_utilities/generatePasswordHash.py
@@ -0,0 +1,29 @@
+from getpass import getpass
+from hashlib import sha1, sha256, sha512
+
+def getPassword():
+    pw = getpass('Enter your password: ')
+    pw2 = getpass('Enter your password for confirmation: ')
+    if pw == pw2:
+        return pw
+    print "Your passwords do not match, please try again!\n"
+    getPassword()
+
+if __name__ == '__main__':
+    print "Warning!! You will not be able to see your password when you enter it..."
+    result = ""
+    password = getPassword()
+    hashObject = sha1()
+    hashObject.update(password)
+    result += "Sha1: " + hashObject.hexdigest() + '\n'
+    hashObject = sha256()
+    hashObject.update(password)
+    result += "Sha256: " + hashObject.hexdigest() + '\n'
+    hashObject = sha512()
+    hashObject.update(password)
+    result += "Sha512: " + hashObject.hexdigest() + '\n'
+    with open('hashedPassword.txt', 'w') as file_:
+        file_.write(result)
+    print "Success!  Please email the 'hashedPassword.txt' file in the current directory to pchan1@stuy.edu.  Thank you!"
+    
+        
---
+++ extra_utilities/html_preprocessor.py
@@ -0,0 +1,36 @@
+from sys import argv
+from os import sep
+
+def patchCodeblockWhiteSpace(filename):
+    with open(filename) as file_:
+        fileContent = file_.readlines()
+    index = 0
+    while index < len(fileContent):
+        line = fileContent[index]
+        if '<codeblock>' in line:
+            fileContent[index] = line.strip('\r\n')
+            if '</codeblock>' not in line:
+                index += 1
+                while index < len(fileContent) - 1 and '</codeblock>' not in fileContent[index]:
+                    line = fileContent[index]
+                    fileContent[index] = line.lstrip()
+                    index += 1
+                    fileContent[index - 1] = fileContent[index - 1].strip('\r\n')
+                    fileContent[index] = fileContent[index].lstrip()
+        index += 1
+    with open('../release/' + filename[filename.rfind(sep) + 1:] + 'c', 'w') as file_:
+        file_.writelines(fileContent)
+    return
+
+def main():
+    for filename in argv[1:]:
+        if filename.endswith('.html'):
+            print 'Processing ' + filename + '...',
+            patchCodeblockWhiteSpace(filename)
+            print 'Success!'
+        else:
+            print filename + ' is not supported!'
+
+if __name__ == '__main__':
+    main()
+
