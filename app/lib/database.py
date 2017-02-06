335ef5de00b9e92272cf892a6a0f45a4647d62c3
Patrick Chan <patrickchan1999@gmail.com>
2017-02-06 15:10:36 -0500

    Cleaned up index page and add developer rank to database

--- app/lib/database.py
+++ app/lib/database.py
@@ -7,24 +7,24 @@ class UserManager:
     def __init__(self, db):
         self.db = client[db]
 
-    def login(self, username, password):
-        result = self.db.users.find_one({
-          'username': username,
-          'passhash': hash_string(password)
-        })
-         
-        if result is None:
-            return False, 'Invalid username or password.'
-        else:
-            return True, 'Successfully logged in!'
-
-    def isRegistered(self, username):
+    def is_registered(self, username):
         result = self.db.users.find_one({
             'username': username
         })
-
         return bool(result)
 
+    def is_admin(self, username):
+        result = self.db.admins.find_one({
+            'username': username
+        })
+        return bool(result)
+
+    def is_developer(self, username):
+        result = self.db.developers.find_one({
+            'username': username
+        })
+        return bool(result)
+    
     def register(self, username, password, confirmationPassword):
         if self.isRegistered(username):
             return False, 'User already exists.'
@@ -35,60 +35,106 @@ class UserManager:
                 'username': username,
                 'passhash': hash_string(password)
             })
-
             return True, 'Successfully registered!'
 
-    def dropUser(self, username):
+    def login(self, username, password):
         result = self.db.users.find_one({
-            'username': username
+          'username': username,
+          'passhash': hash_string(password)
         })
-        if result:
-            self.db.users.remove({
-                'username': username
-            })
-            return True
-        return False
-        
-    def isAdmin(self, username):
-        result = self.db.admins.find_one({
-            'username': username
-        })
-
-        return bool(result)
-            
-    def makeAdmin(self, username):
-        if self.isAdmin(username):
-            return False, 'User is already an admin'
-        result = self.db.users.find_one({
-                'username': username,
-            })
-        if result is not None:            
-            self.db.admins.insert_one({
-                'username': username,
-                'passhash': result['passhash']
-            })
-            return True, 'User is now an admin'
-
-    def authAdmin(self, username, password):
-        result = self.db.admins.find_one({
-            'username': username,
-            'passhash': hash_string(password)
-        })
-
-        if result is None:
+         
+        if not result:
             return False, 'Invalid username or password.'
         else:
             return True, 'Successfully logged in!'
 
-    def dropAdmin(self, username):
+    def make_admin(self, username):
+        if self.isAdmin(username):
+            return False, 'User is already an admin.'
+        result = self.db.users.find_one({
+                'username': username
+            })
+        if result:            
+            self.db.admins.insert_one({
+                'username': username,
+                'passhash': result['passhash']
+            })
+            return True, 'User is now an admin!'
+        else:
+            return False, 'User does not exists.'
+
+    def make_developer(self, username):
+        if self.isDeveloper(username):
+            return False, 'User is already a developer.'
+        result = self.db.users.find_one({
+            'username': username
+        })
+        if result:
+            self.db.developers.insert_one({
+                'username': username,
+                'passhash': result['passhash']
+            })
+            self.makeAdmin(username)
+            return True, 'User is now a developer!'
+        else:
+            return False, 'User does not exists.'
+        
+    def drop_user(self, username):
+        result = self.db.users.find_one({
+            'username': username
+        })
+        if not result:
+            return False, 'User is not found.'
+        else:
+            self.db.users.remove({
+                'username': username
+            })
+            self.dropAdmin(username)
+            self.dropDeveloper(username)
+            return True, 'User is no longer registered!'
+                    
+    def drop_admin(self, username):
         result = self.db.admins.find_one({
             'username': username
         })
 
-        if result is None:
-            return False
+        if not result:
+            return False, 'User is not an admin.'
         else:
             self.db.admins.remove({
                 'username': username
             })
-            return True
+            return True, 'User is no longer an admin!'
+
+    def drop_developer(self, username):
+        result = self.db.developers.find_one({
+            'username': username
+        })
+        if not result:
+            return False, 'User is not a developer.'
+        else:
+            self.db.developers.find_one({
+                'username': username
+            })
+            return True, 'User is no longer a developer!'
+        
+
+    def auth_admin(self, username, password):
+        result = self.db.admins.find_one({
+            'username': username,
+            'passhash': hash_string(password)
+        })
+        if not result:
+            return False, 'Invalid username or password.'
+        else:
+            return True, 'Successfully logged in!'
+
+    def auth_developer(self, username, password):
+        result = self.db.developers.find_one({
+            'username': username,
+            'passhash': hash_string(password)
+        })
+        if not result:
+            return False, 'Invalid username or password.'
+        else:
+            return True, 'Successfully logged in!'
